f32e0d7ca2dd5e4ba30fb364f17d7d7ad9756c5f
iloy1004 <iloy1004ca@gmail.com>
2016-03-19 01:01:52 -0400

    Add enemy's movement

---
+++ 3D Assignment3/Assets/_Script/DinoController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DinoController : MonoBehaviour {
+
+
+
+	public GameObject Dino;
+	public Transform Player;
+
+	private Vector3 _originPosition;
+
+	public int MoveSpeedMax = 5;
+	public int MoveSpeedMin = 3;
+	public int MaxDist = 10;
+	public int MinDist = 5;
+
+
+	// Use this for initialization
+	void Start () {  
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt (Player);
+
+		if (Vector3.Distance (transform.position, Player.position) >= this.MinDist)
+		{
+			transform.position += transform.forward * Random.Range (this.MoveSpeedMin, this.MoveSpeedMax) * Time.deltaTime;
+
+			if (Vector3.Distance (transform.position, Player.position) <= this.MaxDist) 
+			{
+
+			}
+		}
+	}
+
+}
